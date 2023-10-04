d87940829c863d99a9fee4021d596133cee098e9
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-10-04 12:10:11 +0200

    Commit 3

---
+++ Assets/Scripts/RotationController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationController : MonoBehaviour
+{
+    public GameObject PlanetObject;
+    public Vector3 RotationVector;
+
+
+    private void Update()
+    {
+        PlanetObject.transform.Rotate(RotationVector * Time.deltaTime);
+    }
+}
